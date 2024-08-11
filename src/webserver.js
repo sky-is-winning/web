b4f3aaec50ed2ea7e822820f23aed7d2b5f9a230
sky <67554336+sky-is-winning@users.noreply.github.com>
2024-08-11 21:07:43 +0100

    support https?

--- src/webserver.js
+++ src/webserver.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import {publicIpv4} from 'public-ip';
+import { publicIpv4 } from 'public-ip';
 import { EventEmitter } from 'events';
 import Game from './game.js';
 import WWW from './www.js';
+import https from 'https';
+import fs from 'fs';
 
 const PUBLIC_IP = await publicIpv4();
 
@@ -15,26 +17,57 @@ export default class WebServer {
     }
 
     start() {
-        const port = process.argv[2] || 80;
+        const doHTTPS = fs.existsSync('../self.pem') && fs.existsSync('../server-key.pem');
+        const port = process.argv[2] || (doHTTPS ? 443 : 80);
+        
+        if (doHTTPS) {
+            const options = {
+                key: fs.readFileSync('../server-key.pem'),
+                cert: fs.readFileSync('../self.pem'),
+            };
 
-        // Handle all GET and POST requests
-        this.app.all('*', (req, res, next) => {
-            const subdomain = req.headers.host.split('.')[0];
-            switch (subdomain) {
-                case 'play':
-                    this.game.serve('./vanilla-media/play', req, res);
-                    break;
-                case 'media':
-                    this.game.serve('./vanilla-media/media', req, res);
-                    break;
-                default:
-                    this.www.serve(req, res);
-                    break;
-            }
-        });
+            // Create HTTPS server
+            const server = https.createServer(options, this.app);
 
-        this.app.listen(port, () => {
-            console.log(`Server running on port ${port}`);
-        });
+            // Handle all GET and POST requests
+            this.app.all('*', (req, res, next) => {
+                const subdomain = req.headers.host.split('.')[0];
+                switch (subdomain) {
+                    case 'play':
+                        this.game.serve('./vanilla-media/play', req, res);
+                        break;
+                    case 'media':
+                        this.game.serve('./vanilla-media/media', req, res);
+                        break;
+                    default:
+                        this.www.serve(req, res);
+                        break;
+                }
+            });
+
+            server.listen(port, () => {
+                console.log(`HTTPS Server running on port ${port}`);
+            });
+        } else {
+            // Fallback to HTTP server (optional)
+            this.app.all('*', (req, res, next) => {
+                const subdomain = req.headers.host.split('.')[0];
+                switch (subdomain) {
+                    case 'play':
+                        this.game.serve('./vanilla-media/play', req, res);
+                        break;
+                    case 'media':
+                        this.game.serve('./vanilla-media/media', req, res);
+                        break;
+                    default:
+                        this.www.serve(req, res);
+                        break;
+                }
+            });
+
+            this.app.listen(port, () => {
+                console.log(`HTTP Server running on port ${port}`);
+            });
+        }
     }
 }
